af819a06314b805e54ca3f0fa5fe43f40bb31773
Brais Iglesias <braisiglesias17@gmail.com>
2021-11-02 17:50:02 +0100

    Mas cambios

--- GestionDeCursos/app/src/main/java/com/example/e_curso/view/MenuPrincipal.java
+++ GestionDeCursos/app/src/main/java/com/example/e_curso/view/MenuPrincipal.java
@@ -1,9 +1,13 @@
 package com.example.e_curso.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.e_curso.MainActivity;
 import com.example.e_curso.R;
 
 public class MenuPrincipal extends AppCompatActivity {
@@ -13,5 +17,36 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_usuario_activity);
+
+
+
+
+
+
+        this.gestionVerCursos();
+        this.gestionVerCursosApuntados();
+
+    }
+
+    private void gestionVerCursosApuntados() {
+        Button btVerCursos=this.findViewById(R.id.btVerCursosGenerales);
+        btVerCursos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+    }
+
+    private void gestionVerCursos() {
+        Button btVerMisCursos=this.findViewById(R.id.btVerCursosApuntados);
+        btVerMisCursos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
