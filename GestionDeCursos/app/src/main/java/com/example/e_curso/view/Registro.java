e52647ffad9a3426fea9092f41b61d0f8ecc00cf
xiannieto <xjnieto18@esei.uvigo.es>
2021-11-29 16:06:09 +0100

    registro

--- GestionDeCursos/app/src/main/java/com/example/e_curso/view/Registro.java
+++ GestionDeCursos/app/src/main/java/com/example/e_curso/view/Registro.java
@@ -1,17 +1,87 @@
 package com.example.e_curso.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.e_curso.MyApplication;
 import com.example.e_curso.R;
+import com.example.e_curso.core.Usuario;
+import com.example.e_curso.database.DBManager;
+import com.example.e_curso.model.UsuarioFacade;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Registro extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Inicialización del entorno
         setContentView(R.layout.sign_in_activity);
+        DBManager db = ((MyApplication) this.getApplication()).getDBManager();
+        UsuarioFacade uf = new UsuarioFacade(db);
+        Button registrar = this.findViewById(R.id.btSignIn);
+
+        final EditText usuario = this.findViewById(R.id.etuserName);
+        final EditText nombreCompleto = this.findViewById(R.id.etNombreCompleto);
+        final EditText email = this.findViewById(R.id.etMailUsuario);
+        EditText pass = this.findViewById(R.id.etPasswd);
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int solicitud;
+
+                byte[] passBytes = passIntoBytes(pass);
+                solicitud = comprobarSolicitud();
+
+                Usuario nuevoUsuario = new Usuario(usuario.toString(),nombreCompleto.toString(),passBytes,
+                        email.toString(), Usuario.Rol.USER, solicitud);
+                Usuario usuarioFinal = mockUpMethod(nuevoUsuario);
+
+                uf.insertUsuario(usuarioFinal);
+
+                Intent intent = new Intent(Registro.this,MenuPrincipal.class);
+                intent.putExtra("usuario",usuarioFinal);
+                Registro.this.startActivity(intent);
+            }
+        });
+
+
+    }
+
+    public int comprobarSolicitud(){
+        CheckBox checked = this.findViewById(R.id.cbSolicitudDivulgador);
+
+        if(checked.isChecked())
+            return 1;
+        else
+            return 0;
+    }
+
+    private Usuario mockUpMethod(Usuario usuario){
+        byte [] digest;
+        MessageDigest md = null; //genero un resumen de  la contraseña en claro
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        digest = md.digest(usuario.getPass().toString().getBytes(StandardCharsets.UTF_8));
+        return new Usuario(usuario.getUser(), usuario.getNombreCompleto(),
+                digest,usuario.getEmail(), Usuario.Rol.USER,usuario.getSolicitud());
+    }
+
+    private byte[] passIntoBytes(EditText pass){
+        return pass.getText().toString().getBytes(StandardCharsets.UTF_8);
     }
 }
