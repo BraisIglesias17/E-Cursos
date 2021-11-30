90539ff4348926ee784bd469b284d7a5753413ab
Aniado9 <briangmachado@gmail.com>
2021-11-30 19:28:50 +0100

    EliminarCurso, ModificarCurso y strings varios

--- GestionDeCursos/app/src/main/java/com/example/e_curso/view/CrearCurso.java
+++ GestionDeCursos/app/src/main/java/com/example/e_curso/view/CrearCurso.java
@@ -1,6 +1,8 @@
 package com.example.e_curso.view;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -32,7 +34,9 @@ public class CrearCurso extends AppCompatActivity{
     private long idCurso;
     private EditText etFecha;
 
+
     protected void onCreate(Bundle savedInstanceState) {
+
             super.onCreate(savedInstanceState);
             setContentView(R.layout.crear_modificar_curso);
             this.creador=((MyApplication) this.getApplication()).esCreador();
@@ -41,12 +45,15 @@ public class CrearCurso extends AppCompatActivity{
             this.idCurso=this.getIntent().getExtras().getLong("idCurso");
 
             Button bt=(Button) this.findViewById(R.id.btGuardarCambiosCurso);
+            Button btDel=(Button)  this.findViewById(R.id.btEliminarCurso);
+
             if(c==null){
 
                 this.anho= 1900+(new Date().getYear());
                 this.mes=1;
                 this.dia=1;
                 bt.setText("Crear curso");
+                btDel.setVisibility(View.GONE);
                 this.crearSpinner("OTRO");
                 bt.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -66,18 +73,24 @@ public class CrearCurso extends AppCompatActivity{
                     @Override
                     public void onClick(View view) {
                         CrearCurso.this.modifyCurso();
-                        Toast.makeText(CrearCurso.this,"El curso ha sido modificado",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                btDel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        CrearCurso.this.eliminarCurso(c);
+                        //Toast.makeText(CrearCurso.this,"El curso ha sido eliminado correctamente",Toast.LENGTH_SHORT).show();
                     }
                 });
             }
 
-
             this.escogerFecha();
-
 
         }
 
     private void crearSpinner(String porDefecto) {
+
         Spinner spinner = (Spinner) findViewById(R.id.spinnerTematica);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -93,28 +106,7 @@ public class CrearCurso extends AppCompatActivity{
     }
 
     private void addCurso(){
-            EditText txtNombreCurso=(EditText)this.findViewById(R.id.etNombreCurso);
-            EditText txtDescripcion=(EditText)this.findViewById(R.id.etResumenCurso);
-            EditText txtFecha=(EditText)this.findViewById(R.id.etFechaCurso);
-            EditText txtDuracion=(EditText)this.findViewById(R.id.etDuracionCurso);
-            EditText txtNumAsist=(EditText)this.findViewById(R.id.etMaxAsistentes);
-            Spinner txtTematica= (Spinner) this.findViewById(R.id.spinnerTematica);
 
-            String nombre=txtNombreCurso.getText().toString();
-            String descripcion=txtDescripcion.getText().toString();
-            String tematica=(String) txtTematica.getSelectedItem();
-            //Date fecha=this.escogerFecha();
-            Double duracion=Double.parseDouble(txtDuracion.getText().toString());
-            int numAsist=Integer.parseInt(txtNumAsist.getText().toString());
-            long id_Creador=((MyApplication) this.getApplication()).getId_user_logged();
-            Curso toAdd=new Curso(nombre,descripcion,tematica,0,numAsist,this.fecha,duracion,id_Creador);
-
-          CursoFacade cf=new CursoFacade(((MyApplication) this.getApplication()).getDBManager());
-          cf.insertaCurso(toAdd);
-          this.finish();
-        }
-
-    private void modifyCurso(){
         EditText txtNombreCurso=(EditText)this.findViewById(R.id.etNombreCurso);
         EditText txtDescripcion=(EditText)this.findViewById(R.id.etResumenCurso);
         EditText txtFecha=(EditText)this.findViewById(R.id.etFechaCurso);
@@ -125,15 +117,79 @@ public class CrearCurso extends AppCompatActivity{
         String nombre=txtNombreCurso.getText().toString();
         String descripcion=txtDescripcion.getText().toString();
         String tematica=(String) txtTematica.getSelectedItem();
-        Date fecha=this.fecha;
+        //Date fecha=this.escogerFecha();
         Double duracion=Double.parseDouble(txtDuracion.getText().toString());
         int numAsist=Integer.parseInt(txtNumAsist.getText().toString());
         long id_Creador=((MyApplication) this.getApplication()).getId_user_logged();
-        Curso toModify=new Curso(nombre,descripcion,tematica,0,numAsist,fecha,duracion,id_Creador);
+        Curso toAdd=new Curso(nombre,descripcion,tematica,0,numAsist,this.fecha,duracion,id_Creador);
 
         CursoFacade cf=new CursoFacade(((MyApplication) this.getApplication()).getDBManager());
-        cf.updateCurso(toModify,this.idCurso);
-        Toast.makeText(this,"El curso ha sido modificado",Toast.LENGTH_SHORT);
+        cf.insertaCurso(toAdd);
+        CrearCurso.this.finish();
+        Toast.makeText(CrearCurso.this,"El curso ha sido creado correctamente",Toast.LENGTH_SHORT).show();
+    }
+
+    private void modifyCurso(){
+
+        EditText txtNombreCurso=(EditText)this.findViewById(R.id.etNombreCurso);
+        EditText txtDescripcion=(EditText)this.findViewById(R.id.etResumenCurso);
+        EditText txtFecha=(EditText)this.findViewById(R.id.etFechaCurso);
+        EditText txtDuracion=(EditText)this.findViewById(R.id.etDuracionCurso);
+        EditText txtNumAsist=(EditText)this.findViewById(R.id.etMaxAsistentes);
+        Spinner txtTematica= (Spinner) this.findViewById(R.id.spinnerTematica);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Quiere guardar los cambios?")
+                .setTitle("Guardar Cambios");
+
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                String nombre=txtNombreCurso.getText().toString();
+                String descripcion=txtDescripcion.getText().toString();
+                String tematica=(String) txtTematica.getSelectedItem();
+                Date fecha=CrearCurso.this.fecha;
+                Double duracion=Double.parseDouble(txtDuracion.getText().toString());
+                int numAsist=Integer.parseInt(txtNumAsist.getText().toString());
+                long id_Creador=((MyApplication) CrearCurso.this.getApplication()).getId_user_logged();
+                Curso toModify=new Curso(nombre,descripcion,tematica,0,numAsist,fecha,duracion,id_Creador);
+
+                CursoFacade cf=new CursoFacade(((MyApplication) CrearCurso.this.getApplication()).getDBManager());
+                cf.updateCurso(toModify,CrearCurso.this.idCurso);
+                CrearCurso.this.finish();
+                Toast.makeText(CrearCurso.this,"El curso ha sido modificado correctamente",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //CrearCurso.this.finish();
+            }
+        });
+
+        builder.create().show();
+    }
+
+    private void eliminarCurso(Curso c){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Está usted segur@ de que desea eliminar este curso?")
+                .setTitle("Eliminar Curso");
+
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                CursoFacade cf=new CursoFacade(((MyApplication) CrearCurso.this.getApplication()).getDBManager());
+                cf.deleteCurso(c);
+                CrearCurso.this.finish();
+                Toast.makeText(CrearCurso.this,"El curso ha sido eliminado correctamente",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+
+        builder.create().show();
+
     }
 
     private void rellenarDatos(Curso c){
@@ -156,14 +212,12 @@ public class CrearCurso extends AppCompatActivity{
             txtFecha.setText(c.getFechaFormato());
             txtDuracion.setText(Double.toString(c.getDuracion()));
             txtNumAsist.setText(Integer.toString(c.getMaxAsistentes()));
-
     }
 
-
-
     private void escogerFecha() {
+
         this.etFecha=(EditText)this.findViewById(R.id.etFechaCurso);
-        TextView fecha=(TextView) this.findViewById(R.id.tvFechaCursoDetalle);
+        TextView fecha=(TextView) this.findViewById(R.id.etFechaCurso);
         fecha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
