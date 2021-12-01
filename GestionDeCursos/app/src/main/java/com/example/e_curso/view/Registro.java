f60a7f8cd68df3f61f31e9ecb22cc9a2a44407b3
BraisIglesias17 <braisiglesias17@gmail.com>
2021-12-01 23:33:19 +0100

    - Comprobar datos rellenados Sign IN - Comprobar nombreUsuario no existe ya

--- GestionDeCursos/app/src/main/java/com/example/e_curso/view/Registro.java
+++ GestionDeCursos/app/src/main/java/com/example/e_curso/view/Registro.java
@@ -1,6 +1,8 @@
 package com.example.e_curso.view;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,10 +10,13 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.e_curso.MainActivity;
 import com.example.e_curso.MyApplication;
 import com.example.e_curso.R;
+import com.example.e_curso.core.Curso;
 import com.example.e_curso.core.Usuario;
 import com.example.e_curso.database.DBManager;
 import com.example.e_curso.model.UsuarioFacade;
@@ -40,24 +45,44 @@ public class Registro extends AppCompatActivity {
         registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int solicitud;
 
-                byte[] passBytes = Registro.this.encriptarPassword(pass.getText().toString());
-                solicitud = comprobarSolicitud();
+                if(Registro.this.comprobarEntradas()){
 
-                Usuario nuevoUsuario = new Usuario(usuario.getText().toString(),nombreCompleto.getText().toString(),passBytes,
-                        email.getText().toString(), Usuario.Rol.USER, solicitud);
+                    if(!Registro.this.nombreUsuarioOcupado()){
+                        int solicitud;
 
-                uf.insertUsuario(nuevoUsuario);
+                        byte[] passBytes = Registro.this.encriptarPassword(pass.getText().toString());
+                        solicitud = comprobarSolicitud();
 
-                Intent intent = new Intent(Registro.this,MenuPrincipal.class);
+                        Usuario nuevoUsuario = new Usuario(usuario.getText().toString(),nombreCompleto.getText().toString(),passBytes,
+                                email.getText().toString(), Usuario.Rol.USER, solicitud);
 
-                Registro.this.startActivity(intent);
+                        uf.insertUsuario(nuevoUsuario);
+
+                        Intent intent = new Intent(Registro.this, MainActivity.class);
+
+                        Registro.this.startActivity(intent);
+                    }else{
+                        Registro.this.dialogoErrorDatos("Nombre de usuario ocupado");
+                    }
+
+
+                }else{
+                    Registro.this.dialogoErrorDatos("Algún campo es vacío o hay algún error en los datos");
+                }
 
             }
         });
 
 
+    }
+
+    private void dialogoErrorDatos(String msg) {
+        AlertDialog.Builder dlg=new AlertDialog.Builder(this);
+        dlg.setTitle("Error");
+        dlg.setMessage(msg);
+        dlg.setNeutralButton("volver",null);
+        dlg.create().show();
     }
 
     public int comprobarSolicitud(){
@@ -84,5 +109,45 @@ public class Registro extends AppCompatActivity {
 
     private byte[] passIntoBytes(EditText pass){
         return pass.getText().toString().getBytes(StandardCharsets.UTF_8);
+    }
+
+    private boolean comprobarEntradas(){
+        boolean toret=true;
+
+        final EditText usuario = this.findViewById(R.id.etuserName);
+        final EditText nombreCompleto = this.findViewById(R.id.etNombreCompleto);
+        final EditText email = this.findViewById(R.id.etMailUsuario);
+        final EditText passwd = this.findViewById(R.id.etPasswd);
+
+
+        if(usuario.getText()==null || usuario.getText().toString().isEmpty()
+                ||nombreCompleto.getText()==null ||nombreCompleto.getText().toString().isEmpty()
+                || email.getText() == null ||nombreCompleto.getText().toString().isEmpty()
+                || passwd.getText()==null ||nombreCompleto.getText().toString().isEmpty()) {
+            toret=false;
+        }
+
+        return toret;
+    }
+
+    @SuppressLint("Range")
+    private boolean nombreUsuarioOcupado(){
+        boolean toret=false;
+        final EditText usuario = this.findViewById(R.id.etuserName);
+
+        UsuarioFacade uf=new UsuarioFacade(((MyApplication) this.getApplication()).getDBManager());
+
+        Cursor usuarios=uf.getUsuarios();
+        usuarios.moveToFirst();
+
+
+        while (usuarios.isAfterLast() == false && !toret) {
+            String user=usuarios.getString(usuarios.getColumnIndex(DBManager.USUARIO_COLUMN_NAME));
+            if(user.equals(usuario.getText().toString())){
+                toret=true;
+            }
+           usuarios.moveToNext();
+        }
+        return toret;
     }
 }
